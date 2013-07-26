58fdd9551149757284f7b59af0079db37e613527
Lucas Pirola <lucaspirola@gmail.com>
2013-07-26 17:59:28 -0300

    correcao no texto do alerta; centralizacao do alerta; correcao de css do participe que atrapalhava o alerta; espacamento na base do alerta;

--- header.php
+++ header.php
@@ -108,5 +108,5 @@ wp_head();
 		</div> <!-- #wrapper -->
 	</div>
     <div class="wrapper" id="mensagem-alerta">
-        <p>ÚLTIMOS DIAS PARA ENVIO DE PROPOSTA DO PLANO DIRETOR: NÃO PERCA A OFICINA FINAL (SÁBADO 27/07) OU ENVIE ONLINE ATÉ 31/07.</p>
+        <p>ÚLTIMOS DIAS PARA ENVIO DE PROPOSTA DO PLANO DIRETOR: NÃO PERCA A OFICINA FINAL (SÁB. 27/07) OU ENVIE ONLINE ATÉ 31/07</p>
     </div>
--- page-participe-2.php
+++ page-participe-2.php
@@ -22,7 +22,7 @@ Template Name: Participe V.2
 		margin-right: 30px;
 	}
 
-	p {min-height: 70px}
+	#page-participe p {min-height: 70px}
 
 	.col1 .arrow {
 		margin-left: 150px;
@@ -52,14 +52,14 @@ Template Name: Participe V.2
 		<p>Antes de escolher uma das opções, consulte as <a id="inline" href="http://gestaourbana.prefeitura.sp.gov.br/regras_b">regras</a></p>
 
 		<div class="col1 col">
-			<p class="p1">Aponte no mapa da capital o que está bom ou precisa melhorar na sua região. De ideias como intervenções urbanísticas, corredores urbanos, parques lineares, etc.</p> 
+			<p class="p1">Aponte no mapa da capital o que está bom ou precisa melhorar na sua região. De ideias como intervenções urbanísticas, corredores urbanos, parques lineares, etc.</p>
 			<!-- <p style="position:absolute; height:1px">Em breve </p> -->
 			<img src="<?php bloginfo('template_url') ?>/images/participe/arrow_1.png" class="arrow">
 			<br />&nbsp;<a href="http://mapa.gestaourbana.prefeitura.sp.gov.br/">
 				<img class="big-btn" src="<?php bloginfo('template_url') ?>/images/participe/btn-mapaeamento.jpg">
 			</a>
 		</div>
-		
+
 		<div class="col2 col">
 			<p class="p2">Faça sua contribuição diretamente para o Plano Diretor Estratégico preenchendo o formulário de envio de proposta.</p>
 			<!-- <p style="position:absolute; height:1px">Em breve </p> -->
@@ -70,7 +70,7 @@ Template Name: Participe V.2
 		</div>
 
 		<div class="col3 col">
-			<p class="p3">Acompanhe a agenda de atividades e participe através de oficinas de propostas na sua região.</p>			
+			<p class="p3">Acompanhe a agenda de atividades e participe através de oficinas de propostas na sua região.</p>
 			<img src="<?php bloginfo('template_url') ?>/images/participe/arrow_3.png" class="arrow">
 			<br />&nbsp;<a href="http://gestaourbana.prefeitura.sp.gov.br/evento/">
 				<img class="big-btn" src="<?php bloginfo('template_url') ?>/images/participe/btn-agenda.jpg">
@@ -81,19 +81,19 @@ Template Name: Participe V.2
 			<p><br /><br />ATENÇÃO - REGRAS<br /><br />
 			Para que todos possam participar de forma produtiva é necessário pactuar algumas regras. De um lado, a prefeitura se compromete em analisar e dar a devolutiva de cada proposta apresentada, mas de outro, é necessário que todos participem respeitando os demais grupos e agentes da cidade e construindo propostas consistentes com os objetivos do PDE. Dessa forma, a Prefeitura se compromete a analisar e fazer a devolutiva das propostas desta 2ª etapa que respeitem o exposto abaixo:<br /><br />
 			• Só serão aceitas contribuições identificadas, inclusive com um endereço eletrônico (email) para possível confirmação;<br />
-			• As contribuições devem ser encaminhadas em formulários próprios, devidamente 
-			preenchidos. Os formulários serão distribuídos nas oficinas participativas 
+			• As contribuições devem ser encaminhadas em formulários próprios, devidamente
+			preenchidos. Os formulários serão distribuídos nas oficinas participativas
 			realizadas nas Subprefeituras e também estarão disponíveis online até o término da 2ª Etapa de Revisão Participativa (30 de julho de 2013);<br />
-			• Só será aceita uma contribuição por formulário. Quando o formulário possuir mais de 
-			uma contribuição, só a primeira será considerada. Não há limite de formulários por 
+			• Só será aceita uma contribuição por formulário. Quando o formulário possuir mais de
+			uma contribuição, só a primeira será considerada. Não há limite de formulários por
 			pessoa;<br />
 			• Serão desconsideradas contribuições com mensagens ofensivas a quem quer que seja.<br />
 			• Não serão aceitas contribuições com citações sem as fontes.<br /><br />
 			RECOMENDAÇÕES<br /><br />
-			• Faça um texto enxuto com a ideia de sua proposta, evite textos longos com várias 
-			argumentações em uma mesma contribuição (lembrando que, em contribuições com mais 
+			• Faça um texto enxuto com a ideia de sua proposta, evite textos longos com várias
+			argumentações em uma mesma contribuição (lembrando que, em contribuições com mais
 			de uma ideia, só a primeira será considerada);<br />
-			• Ler um texto sem pontuação gera desconforto, e, além disso, aumenta as chances dele 
+			• Ler um texto sem pontuação gera desconforto, e, além disso, aumenta as chances dele
 			ser mal interpretado. Portanto, confira se o texto está de acordo com sua intenção;<br />
 			• Evite enviar mensagens exclusivamente em maiúsculas;<br />
 			• Não use abreviações e evite o uso da linguagem "internetês".<br /></p>
