e844380defda46629850eaa46d5cf2fbca4ac35d
d816352 <d816352@SMDUGBC67.rede.sp>
2014-08-15 11:54:38 -0300

    layout novo

--- wp-content/themes/gestaourbana-1.2/page-sem-sidebar-sem-titulo.php
+++ wp-content/themes/gestaourbana-1.2/page-sem-sidebar-sem-titulo.php
@@ -101,6 +101,68 @@ jQuery(document).ready(function()
     });
 });
 
+jQuery(document).ready(function()
+{
+    // Set up variables
+    var $el, $parentWrap, $otherWrap, 
+        $allTitles = jQuery("dt").css({
+            padding: 5, // setting the padding here prevents a weird situation, where it would start animating at 0 padding instead of 5
+            "cursor": "pointer" // make it seem clickable
+        }),
+        $allCells = jQuery("dd").css({
+            position: "relative",
+            top: -1,
+            left: 0,
+            display: "none" // info cells are just kicked off the page with CSS (for accessibility)
+        });
+    
+    // clicking on titles does stuff
+    jQuery("#accordion2").delegate("dt", "click", function() {
+        
+        // cache this, as always, is good form
+        $el = jQuery(this);
+        
+        // if this is already the active cell, don't do anything
+        if (!$el.hasClass("current")) {
+        
+            $parentWrap = $el.parent().parent();
+            $otherWraps = jQuery(".info-col").not($parentWrap);
+            
+            // remove current cell from selection of all cells
+            $allTitles = jQuery("dt").not(this);
+            
+            // close all info cells
+            $allCells.slideUp();
+            
+            // return all titles (except current one) to normal size
+            $allTitles.animate({
+            });
+            
+            // animate current title to larger size            
+            $el.animate({
+            }).next().slideDown();
+            
+            // make the current column the large size
+            $parentWrap.animate({
+                
+            }).addClass("curCol");
+            
+            // make other columns the small size
+            $otherWraps.animate({
+                
+            }).removeClass("curCol");
+            
+            // make sure the correct column is current
+            $allTitles.removeClass("current");
+            $el.addClass("current");  
+        
+        }
+        
+    });
+    
+    jQuery("#starter").trigger("click");
+});
+
 </script>
 
 <?php get_footer(); ?>
